c536b1469acb4aa25f110ce1aa9a71259b1a7056
Flaikos <flaikebs@gmail.com>
2023-10-19 15:54:55 +0200

    un peu de changement

--- assets/js/appActu.js
+++ assets/js/appActu.js
@@ -33,7 +33,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== Effet flou ===============*/
 const blurHeader = () =>{
     const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('blur-header')
+    this.scrollY >= 30 ? header.classList.add('blur-header')
         : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
